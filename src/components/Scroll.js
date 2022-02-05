18c1b919d51cea939630e0e20a6eb28054405810
Thoutireddy, SaiShiva <saishiva.thoutireddy@oracle.com>
2022-02-05 16:25:15 +0530

    Updates

--- src/components/Scroll.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Scroll(props) {
-    return (<div style={{overflowY: 'scroll', border: '1px solid black', height: '400px'}}>
-        {props.children}
-    </div>)
-}
-
-export default Scroll;
